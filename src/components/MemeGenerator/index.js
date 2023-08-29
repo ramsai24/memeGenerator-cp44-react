67e55b06321aced2eb7603a5b6f1a1b53cba46c8
jnrsai24 <jnrsai24@gmail.com>
2023-08-29 04:25:23 +0000

    frame is done and event listening should occur

--- src/components/MemeGenerator/index.js
+++ src/components/MemeGenerator/index.js
@@ -1,3 +1,6 @@
+import {Component} from 'react'
+import {Container, Heading} from './styledComponents'
+
 const fontSizesOptionsList = [
   {
     optionId: '8',
@@ -28,4 +31,64 @@ const fontSizesOptionsList = [
     displayText: '32',
   },
 ]
+
 // Write your code here
+class MemeGenerator extends Component {
+  state = {
+    imageUrl: '',
+    topText: '',
+    bottomText: '',
+    fontSize: fontSizesOptionsList[0].displayText,
+  }
+
+  updateImageUrl = event => this.setState({imageUrl: event.target.value})
+
+  updateTopText = event => this.setState({topText: event.target.value})
+
+  updateBottomText = event => this.setState({bottomText: event.target.value})
+
+  updateFontSize = event => this.setState({fontSize: event.target.value})
+
+  onSubmitForm = event => {
+    event.preventDefault()
+  }
+
+  render() {
+    const {imageUrl, topText, bottomText, fontSize} = this.state
+    console.log(topText, bottomText)
+    return (
+      <div>
+        <form onSubmit={this.onSubmitForm}>
+          <label htmlFor="imageUrl">Image URL</label>
+          <input type="text" id="imageUrl" onClick={this.updateImageUrl} />
+
+          <label htmlFor="topText">Top Text</label>
+          <input type="text" id="topText" onClick={this.updateTopText} />
+
+          <label htmlFor="bottomText">Bottom Text</label>
+          <input type="text" id="bottomText" onClick={this.updateBottomText} />
+
+          <label htmlFor="fontSize">Font Size</label>
+          <select id="fontSize" onClick={this.updateFontSize}>
+            {fontSizesOptionsList.map(each => (
+              <option key={each.optionId}>{each.displayText}</option>
+            ))}
+          </select>
+
+          <button type="submit" data-testid="meme">
+            Generate
+          </button>
+        </form>
+
+        <div>
+          <Container backgroundImage={imageUrl}>
+            <Heading fontSize={fontSize}>{topText}</Heading>
+            <Heading fontSize={fontSize}>{bottomText}</Heading>
+          </Container>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default MemeGenerator
--- src/components/MemeGenerator/styledComponents.js
+++ src/components/MemeGenerator/styledComponents.js
@@ -1,1 +1,13 @@
 // Style your components here
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  background-image: ${props => props.url};
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+`
+export const Heading = styled.h1`
+  font-size: ${props => props.fontSize};
+`
